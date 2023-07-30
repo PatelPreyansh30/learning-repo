3fa6a9c66b0e2e9abd64d65239120d44a6157497
Preyansh Patel <patelpreyansh3011@gmail.com>
2023-07-30 19:41:52 +0530

    closure in javascript

--- javascript_concepts/hoisting.js
+++ javascript_concepts/hoisting.js
@@ -1,11 +1,11 @@
 // Reference: https://www.w3schools.com/js/js_hoisting.asp
 
-// x = 10;
-// y = 20;
+x = 10;
+y = 20;
 
-// console.log(x + y);
+console.log(x + y);
 
-// var x, y;
+var x, y;
 
 // Let and Const not support hoisting
 a = 5;
