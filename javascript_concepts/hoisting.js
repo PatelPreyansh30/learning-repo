9c9f0f4f0b7be22f560cdb55ceac784f0758f156
Preyansh Patel <patelpreyansh3011@gmail.com>
2023-07-30 19:33:44 +0530

    hoisting in javascript

---
+++ javascript_concepts/hoisting.js
@@ -0,0 +1,13 @@
+// Reference: https://www.w3schools.com/js/js_hoisting.asp
+
+// x = 10;
+// y = 20;
+
+// console.log(x + y);
+
+// var x, y;
+
+// Let and Const not support hoisting
+a = 5;
+console.log(a);
+let a;
