995beb7a362d8d8717f62a41fa71641c9012aa35
Preyansh Patel <patelpreyansh3011@gmail.com>
2023-07-31 22:33:28 +0530

    async await in javascript

--- javascript_concepts/async_await.js
+++ javascript_concepts/async_await.js
@@ -1,2 +1,15 @@
-// Reference:
+// Reference: https://www.geeksforgeeks.org/async-await-function-in-javascript/
 
+let asyncDemo = async () => {
+  console.log("Hello 1");
+
+  setTimeout(() => {
+    console.log("Hello");
+  }, 0);
+
+  console.log("Hello 2");
+};
+
+console.log(1);
+asyncDemo();
+console.log(2);
